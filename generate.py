d8e45281ad30d3f882780418cfd9eab19648977d
divyadharshiniv2007-crypto <divyadharshiniv2007@gmail.com>
2026-02-16 11:21:09 +0530

    Initial backend upload

---
+++ generate.py
@@ -0,0 +1,20 @@
+import sys
+from audiocraft.models import MusicGen
+from audiocraft.data.audio import audio_write
+import os
+
+prompt = sys.argv[1]
+
+model = MusicGen.get_pretrained("facebook/musicgen-small")
+model.set_generation_params(duration=15)
+
+audio = model.generate([prompt])
+
+output_path = os.path.join(os.path.dirname(__file__), "output")
+
+audio_write(
+    output_path,
+    audio[0].cpu(),
+    model.sample_rate,
+    strategy="loudness"
+)
---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const { exec } = require("child_process");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+// Test route
+app.get("/test", (req, res) => {
+  res.send("Backend working");
+});
+
+// AI music generation route
+app.post("/generate", (req, res) => {
+  const prompt = req.body.prompt;
+
+  const scriptPath = path.join(__dirname, "generate.py");
+  const audioPath = path.join(__dirname, "output.wav");
+
+  exec(`python "${scriptPath}" "${prompt}"`, (error) => {
+    if (error) {
+      console.error(error);
+      return res.status(500).send("AI generation failed");
+    }
+
+    res.setHeader("Content-Type", "audio/wav");
+    res.sendFile(audioPath);
+  });
+});
+
+app.get("/", (req, res) => {
+  res.send("Backend is Live on Render 🚀");
+});
+
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT);
+}); 
